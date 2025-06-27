19a6cc4c42a6ac594411cefa93640c95e0a16bfd
Denis Dvali <2803994+denho@users.noreply.github.com>
2025-06-27 18:03:40 +0000

    Add support for image path prefixing for image optimization needs

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,9 +1,10 @@
 import NextImage, { ImageProps } from 'next/image'
 
 const basePath = process.env.BASE_PATH
+const imagePathPrefix = process.env.IMAGE_PATH_PREFIX || ''
 
 const Image = ({ src, ...rest }: ImageProps) => (
-  <NextImage src={`${basePath || ''}${src}`} {...rest} />
+  <NextImage src={`${imagePathPrefix}${basePath || ''}${src}`} {...rest} />
 )
 
 export default Image
