636b88a042f95a8a52fa44b4d7b8cf6c6d667a52
Denis Dvali <2803994+denho@users.noreply.github.com>
2026-05-01 21:14:11 +0400

    Improve feature items layout

--- components/sections/items/default.tsx
+++ components/sections/items/default.tsx
@@ -99,7 +99,7 @@ export default function Items({
           {title}
         </h2>
         {items !== false && items.length > 0 && (
-          <div className="grid auto-rows-fr grid-cols-2 gap-0 sm:grid-cols-3 sm:gap-4 lg:grid-cols-4">
+          <div className="grid grid-cols-1 gap-1 sm:grid-cols-2 sm:gap-3 lg:grid-cols-3 lg:gap-5">
             {items.map((item, index) => (
               <Item key={index}>
                 <ItemTitle className="flex items-center gap-2">
